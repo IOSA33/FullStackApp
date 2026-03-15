542766d981ae201beca736ba1b463d81fdf80827
IOSA33 <sedinkin33@gmail.com>
2026-03-15 18:27:43 +0200

    2.11 puhelinluettelo step6

--- Osa2/puhelinluettelo/src/App.jsx
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ valueToFind, onChange}) => {
   return (
@@ -62,12 +63,7 @@ const PersonForm = ({ addNote, newName, newPhoneNumber, handleNameChange, handle
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
   const [findByName, setFindByName] = useState('')
@@ -88,6 +84,12 @@ const App = () => {
     setNewName('')
     setNewPhoneNumber('')
   }
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPersons(response.data)
+    })
+  },[])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
