f74dea2e79a5699c8a5b923362dc1e04b4a036d8
IOSA33 <sedinkin33@gmail.com>
2026-08-22 15:01:58 +0300

    6.2 - 6.6 done

---
+++ Osa6/zustand-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,27 @@
+
+import { useAnecdoteActions } from '../store'
+
+const App = () => {
+  const { like, add } = useAnecdoteActions()
+
+  const addAnecdote = (e) => {
+    e.preventDefault()
+    const content = e.target.anecdote.value
+    add(content)
+    e.target.reset()
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name='anecdote' />
+        </div>
+        <button type='submit'>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
---
+++ Osa6/zustand-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,28 @@
+import { useAnecdotes, useAnecdoteActions } from '../store'
+
+const AnecdoteList = () => {
+    const anecdotes = useAnecdotes()
+    const sorted = anecdotes.toSorted((a,b) => b.votes - a.votes)
+    const { like, add } = useAnecdoteActions()
+
+    const vote = id => {
+        console.log('vote', id)
+        like(id)
+    }
+
+    return (
+        <div>
+        {sorted.map(anecdote => (
+            <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+                has {anecdote.votes}
+                <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
+            </div>
+        ))}
+        </div>
+    )
+}
+
+export default AnecdoteList
